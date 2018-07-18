a1d66ee09d07640af79ee1546913a743f9e40f5d
Alexey Prokhorov <megahertz@l96.ru>
2018-07-19 01:06:25 +0500

    Update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-REPO_URL=http://svn.freepascal.org/svn/fpc/trunk
-VERSION=3.1.1
+REPO_URL="http://svn.freepascal.org/svn/fpc/trunk"
+VERSION="3.1.1"
 
-set -e
+set -ex
 
 read_revision() {
   local revision
@@ -16,22 +16,22 @@ read_revision() {
     revision="HEAD"
   fi
 
-  echo revision
+  echo "${revision}"
 }
 
 old_revision="$(read_revision)"
-new_revision="$(svn info ${REPO_URL} --show-item revision)"
+new_revision="$(svn info ${REPO_URL} --show-item revision | tail -n 1)"
 
 if [ "${old_revision}" == "${new_revision}" ]; then
   echo "There are no new revisions in trunk"
   exit 0
 fi
 
-echo ${new_revision} > CHANGELOG
+echo "${new_revision}" > CHANGELOG
 svn log ${REPO_URL} -r ${old_revision}:HEAD | tail -n 50 >> CHANGELOG
 
 git tag "${VERSION}-${new_revision}"
 git add CHANGELOG
 git commit -m "Build ${VERSION}-${new_revision}"
-git push "${VERSION}-${new_revision}"
-git push
+git push origin "${VERSION}-${new_revision}"
+git push origin master
