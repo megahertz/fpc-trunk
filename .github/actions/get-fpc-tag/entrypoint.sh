ed92722fe2c4150f94153e995660f0a843ccd3bb
Alexey Prokhorov <megahertz@l96.ru>
2019-12-02 18:24:42 +0500

    Update entrypoint.sh

--- .github/actions/get-fpc-tag/entrypoint.sh
+++ .github/actions/get-fpc-tag/entrypoint.sh
@@ -2,24 +2,22 @@
 
 set -e
 
-env
-
-
 get_fpc_revision() {
-  svn info ${REPO_URL} --show-item revision | tail -n 1
+  svn info ${INPUT_REPO_URL} --show-item revision | tail -n 1
 }
 
 get_fpc_version() {
- curl "${REPO_URL}/Makefile.fpc" \
+ curl "${INPUT_REPO_URL}/Makefile.fpc" \
    | grep version= \
    | head -n 1 \
    | sed -E 's/version=([0-9.]+)/\1/'
 }
 
 main() {
-  #local revision; revision="$(get_fpc_revision)"
-  #local version; version="$(get_fpc_version)"
-  #local tag="${version}-${revision}"
-
-  echo "repo: ${INPUT_SVN_REPOSITORY}"
+  local revision; revision="$(get_fpc_revision)"
+  local version; version="$(get_fpc_version)"
+  local tag="${version}-${revision}"
+  echo "::set-output name=revision::${revision}"
+  echo "::set-output name=version::${version}"
+  echo "::set-output name=tag::${revision}-${version}"
 }
